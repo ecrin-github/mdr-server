b0b16953d233ac746bed19d872e072f01a3fb5e9
Sergei Gorianin <frequenteen@gmail.com>
2021-06-14 22:55:21 +0200

    Update StudyCharacteristicsRequest.cs

--- mdr-server/Contracts/v1/Requests/Query/StudyCharacteristicsRequest.cs
+++ mdr-server/Contracts/v1/Requests/Query/StudyCharacteristicsRequest.cs
@@ -1,11 +1,12 @@
+#nullable enable
 namespace mdr_server.Contracts.v1.Requests.Query
 {
     public class StudyCharacteristicsRequest : BaseQueryRequest
     {
-        public string TitleContains { get; set; }
-        
-        public string LogicalOperator { get; set; }
-        
-        public string TopicsInclude { get; set; }
+        public string TitleContains { get; set; } = null!;
+
+        public string LogicalOperator { get; set; } = null!;
+
+        public string? TopicsInclude { get; set; }
     }
 }
